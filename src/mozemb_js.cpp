b10b534aea9f1dbd4e79d9457fcc9b1a87385bff
Simon Vogl <simon.vogl@beko.at>
2010-06-25 15:10:52 +0200

    let mozembed be able to quit - based on monitoring the status msg

--- src/mozemb_js.cpp
+++ src/mozemb_js.cpp
@@ -38,7 +38,8 @@ typedef struct _MozEmb {
     GtkMozEmbed *embed;
     char* name; ///< widget?
     JSContext *cx; ///< our js object
-    JSObject *obj; ///< our context
+    JSObject *obj; ///< self
+    JSFunction *cb; ///< callback function, if any
 } MozEmbData;
 
 /*************************************************************************************************/
@@ -57,59 +58,97 @@ static void link_message_cb(GtkMozEmbed *embed, gpointer data) {
 }
 
 static void js_status_cb(GtkMozEmbed *embed, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+    MozEmbData* moz = (MozEmbData*) data;
 
-    cout << "js_status_c callback!" << endl;
     char* status = gtk_moz_embed_get_js_status(embed);
-    cout << status << endl;
-    g_free(status);
+    cout << "js_status_c callback!" << status << endl;
+
+    if (moz->cb == 0 ) {
+        cout << "js_status no cb\n";
+        return;
+    }
+
+    JS_BeginRequest(moz->cx);
+#if 0
+    jsval funcv;
+    JS_GetProperty(moz->cx, moz->obj, "statusCb", &funcv);
+    if (!JSVAL_IS_OBJECT(funcv)) {
+        cerr << "func no obj"<<endl;
+        JS_EndRequest(moz->cx);
+        g_free(status);
+        return;
+    }
+    JSObject * func = JSVAL_TO_OBJECT(funcv);
+    if (!JS_ObjectIsFunction(moz->cx, func)) {
+        cerr << "obj is no func"<<endl;
+        JS_EndRequest(moz->cx);
+        g_free(status);
+        return;
+    };
+    JSFunction* cb = JS_ValueToFunction(moz->cx, funcv);
+    JSString* str = JS_NewString(moz->cx, status, strlen(status));
+    jsval args = STRING_TO_JSVAL(str);
+    jsval ret;
+    JSBool ok = JS_CallFunction(moz->cx, moz->obj, cb, 1, &args, &ret);
+#endif
+    JSString* str = JS_NewString(moz->cx, status, strlen(status));
+    jsval args = STRING_TO_JSVAL(str);
+    jsval ret;
+    JSBool ok = JS_CallFunction(moz->cx, moz->obj, moz->cb, 1, &args, &ret);
+            //JS_CallFunctionName(dta->cx, dta->obj, "statusCb", 1, &args, &ret);
+    if (!ok) {
+        cerr << "Error: Callback did not work out!" << endl;
+    }
+
+    JS_EndRequest(moz->cx);
+    //g_free(status); -> transferred to js
 }
 
 static void location_changed_cb(GtkMozEmbed *embed, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
 
     cout << "location_changed_cb callback!" << endl;
 }
 
 static void title_changed_cb(GtkMozEmbed *embed, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
 
     cout << "title_changed_cb callback!" << endl;
 }
 
 static void progress_change_cb(GtkMozEmbed *embed, gint cur, gint max, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
 
     cout << "progress_change_cb callback!" << cur << "/" << max << endl;
 }
 
 static void net_state_change_cb(GtkMozEmbed *embed, gint flags, guint status, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
 
     cout << "net_state_change_cb callback! " << status << endl;
 }
 
 static void load_started_cb(GtkMozEmbed *embed, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
 
     cout << "load_started_cb callback!" << endl;
 }
 
 static void load_finished_cb(GtkMozEmbed *embed, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
 
     cout << "load_finished_cb callback!" << endl;
 }
 
 static void new_window_cb(GtkMozEmbed *embed, GtkMozEmbed **retval, guint chromemask, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
 
     cout << "new_window_cb callback!" << hex << chromemask << dec << endl;
     *retval = 0;
 }
 
 static void visibility_cb(GtkMozEmbed *embed, gboolean visibility, gpointer data) {
-    MozEmbData* dta = (MozEmbData*) data;
+//    MozEmbData* dta = (MozEmbData*) data;
     cout << "visibility_cb callback!" << endl;
 }
 
@@ -120,11 +159,16 @@ static void destroy_brsr_cb(GtkMozEmbed *embed, gpointer data) {
     JS_BeginRequest(dta->cx);
     jsval args;
     jsval ret;
-    JSBool ok = JS_CallFunctionName(dta->cx, dta->obj, "quit", 0, &args, &ret);
+    JSBool ok = JS_CallFunctionName(dta->cx, dta->obj, "quitCb", 0, &args, &ret);
     if (!ok) {
         cerr << "Error: Callback did not work out!" << endl;
     }
     JS_EndRequest(dta->cx);
+}
+
+static gint destroy_cb(GtkMozEmbed *embed, gpointer data) {
+    MozEmbData* dta = (MozEmbData*) data;
+    cout << "destroy callback!" << endl;
 }
 
 static gint open_uri_cb(GtkMozEmbed *embed, const char *uri, gpointer data) {
@@ -142,27 +186,45 @@ static void init_gtk_stuff(MozEmbData* moz) {
     cerr << "init_gtk 1d " << endl;
     GtkWidget* m = gtk_moz_embed_new();
 
-    gtk_widget_set_size_request(m, 600, 400);
+    gtk_widget_set_size_request(m, 800, 600);
+
+    gtk_container_add(GTK_CONTAINER(window), m);
+
+    int ret = 0;
 
     g_signal_connect(m, "net_stop",
             (GCallback) load_finished_cb, (gpointer) moz);
-    g_signal_connect(m, "net_start",
-            (GCallback) load_started_cb, (gpointer) moz);
+    cerr << "ret " << ret << endl;
+    g_signal_connect(GTK_OBJECT(m), "net_start",
+            G_CALLBACK(load_started_cb), (gpointer) moz);
+    cerr << "ret " << ret << endl;
     g_signal_connect(m, "net_state",
             (GCallback) net_state_change_cb, (gpointer) moz);
+    cerr << "ret " << ret << endl;
     g_signal_connect(m, "progress",
             (GCallback) progress_change_cb, (gpointer) moz);
+    cerr << "ret " << ret << endl;
 
     g_signal_connect(m, "js_status",
             (GCallback) js_status_cb, (gpointer) moz);
+    cerr << "ret " << ret << endl;
 
-    g_signal_connect(m, "destroy_browser",
-            (GCallback) destroy_brsr_cb, (gpointer) moz);
-    g_signal_connect(m, "new_window",
+    ret = g_signal_connect(GTK_OBJECT(m), "destroy",
+            G_CALLBACK(destroy_cb), moz);
+    cerr << "ret d " << ret << endl;
+
+    ret = g_signal_connect(GTK_OBJECT(window), "destroy",
+            G_CALLBACK(destroy_cb), moz);
+    cerr << "ret w " << ret << endl;
+
+    ret = g_signal_connect(m, "new_window",
             (GCallback) new_window_cb, (gpointer) moz);
+    cerr << "ret n " << ret << endl;
 
-    gtk_container_add(GTK_CONTAINER(window), m);
-    //    gtk_window_fullscreen( GTK_WINDOW(window) );
+    ret = g_signal_connect(GTK_OBJECT(m), "destroy_browser",
+            G_CALLBACK(destroy_brsr_cb), (gpointer) moz);
+    cerr << "ret x " << ret << endl;
+
     gtk_window_set_decorated(GTK_WINDOW(window), 0);
 
     gtk_widget_show(m);
@@ -181,7 +243,8 @@ static JSBool MozEmbConstructor(JSContext *cx, JSObject *obj, uintN argc, jsval 
         moz = (MozEmbData*) calloc(sizeof (MozEmbData), 1);
         moz->cx = cx;
         moz->obj = obj;
-
+        moz->cb = 0;
+        
         if (!JS_SetPrivate(cx, obj, moz))
             return JS_FALSE;
 
@@ -199,6 +262,29 @@ static void MozEmbDestructor(JSContext *cx, JSObject *obj) {
         free(moz->name);
         free(moz);
     }
+}
+
+static JSBool MozEmb_set_callback(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    printf("object is in mainloop: %p\n", obj);
+
+    fail_if((argc != 1), "must pass one argument ( callback(str) )!\n");
+
+    MozEmbData* moz = (MozEmbData *) JS_GetPrivate(cx, obj);
+
+    if (!JSVAL_IS_OBJECT(argv[0])) {
+        cerr << "func no obj"<<endl;
+        return JS_FALSE;
+    }
+    JSObject * func = JSVAL_TO_OBJECT(argv[0]);
+    if (!JS_ObjectIsFunction(moz->cx, func)) {
+        cerr << "obj is no func"<<endl;
+        return JS_FALSE;
+    };
+
+    JSFunction* cb = JS_ValueToFunction(moz->cx, argv[0]);
+    moz->cb = cb;
+
+    return JS_TRUE;
 }
 
 static JSBool MozEmb_load(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
@@ -243,6 +329,16 @@ static JSBool MozEmb_go_back(JSContext *cx, JSObject *obj, uintN argc, jsval *ar
     return JS_TRUE;
 }
 
+static JSBool MozEmb_go_forward(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    fail_if((argc != 0), "must not pass an argument!\n");
+
+    MozEmbData* moz = (MozEmbData *) JS_GetPrivate(cx, obj);
+
+    gtk_moz_embed_can_go_forward(moz->embed);
+
+    return JS_TRUE;
+}
+
 static JSBool MozEmb_reload(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
     printf("mozembed.reload() \n");
 
@@ -283,20 +379,18 @@ static JSBool GtkWin_unfullscreen(JSContext *cx, JSObject *obj, uintN argc, jsva
 static JSFunctionSpec _MozEmbFunctionSpec[] = {
     { "load", MozEmb_load, 0, 0, 0},
     { "stop", MozEmb_stop, 0, 0, 0},
-    { "reload", MozEmb_reload, 0, 0, 0},
     { "back", MozEmb_go_back, 0, 0, 0},
+    { "forward", MozEmb_go_forward, 0, 0, 0},
+    { "reload", MozEmb_reload, 0, 0, 0},
+    { "setCallback", MozEmb_set_callback, 0, 0, 0},
     { "fullscreen", GtkWin_fullscreen, 0, 0, 0},
     { "unfullscreen", GtkWin_unfullscreen, 0, 0, 0},
     { 0, 0, 0, 0, 0}
     /*
     http://www.mozilla.org/unix/gtk-embedding.html#function_reference
 
-x   extern void         gtk_moz_embed_load_url         (GtkMozEmbed *embed, const char *url);
-x   extern void         gtk_moz_embed_stop_load        (GtkMozEmbed *embed);
     extern gboolean     gtk_moz_embed_can_go_back      (GtkMozEmbed *embed);
     extern gboolean     gtk_moz_embed_can_go_forward   (GtkMozEmbed *embed);
-    extern void         gtk_moz_embed_go_back          (GtkMozEmbed *embed);
-    extern void         gtk_moz_embed_go_forward       (GtkMozEmbed *embed);
     extern void         gtk_moz_embed_render_data      (GtkMozEmbed *embed,
                                                         const char *data, guint32 len,
                                                         const char *base_uri, const char *mime_type);
@@ -311,15 +405,6 @@ x   extern void         gtk_moz_embed_stop_load        (GtkMozEmbed *embed);
     extern char        *gtk_moz_embed_get_location     (GtkMozEmbed *embed);
     extern void         gtk_moz_embed_set_chrome_mask  (GtkMozEmbed *embed, guint32 flags);
     extern guint32      gtk_moz_embed_get_chrome_mask  (GtkMozEmbed *embed);
-    
-    static funcs:
-
-    extern GtkType      gtk_moz_embed_get_type         (void);
-
-    ///-> constructor
-    extern GtkWidget   *gtk_moz_embed_new              (void);
-    extern void         gtk_moz_embed_set_comp_path    (char *aPath);
-
      */
 };
 
@@ -346,7 +431,9 @@ static JSBool MozEmb_s_mainloop(JSContext *cx, JSObject *obj, uintN argc, jsval 
 
     MozEmbData* moz = (MozEmbData *) JS_GetPrivate(cx, obj);
 
+    gtk_moz_embed_push_startup();
     gtk_main();
+    gtk_moz_embed_pop_startup();
 
     return JS_TRUE;
 }
@@ -367,6 +454,19 @@ static JSFunctionSpec _MozEmbStaticFunctionSpec[] = {
     { 0, 0, 0, 0, 0}
 };
 
+static GtkMozEmbedSingle *single = 0;
+
+void new_window_orphan_cb(GtkMozEmbedSingle *embed,
+        GtkMozEmbed **retval, guint chromemask,
+        gpointer data) {
+    g_print("new_window_orphan_cb\n");
+    g_print("chromemask is %d\n", chromemask);
+    //  TestGtkBrowser *newBrowser = new_gtk_browser(chromemask);
+    //  *retval = GTK_MOZ_EMBED(newBrowser->mozEmbed);
+    *retval = 0;
+    g_print("new browser is %p\n", (void *) * retval);
+}
+
 static JSBool initEnvironment(void) {
     int argc = 0;
     char** argv = {NULL};
@@ -374,13 +474,6 @@ static JSBool initEnvironment(void) {
     gtk_init(&argc, &argv);
 
     cerr << "initEnv 10a " << endl;
-#if 0
-    gtk_moz_embed_set_comp_path("/usr/lib/xulrunner-1.9.1.8/components/");
-    cerr << "initEnv 11a " << endl;
-    gtk_moz_embed_set_profile_path("/tmp", "web_browser_user");
-    cerr << "initEnv 12a " << endl;
-
-#else
 
     static const GREVersionRange greVersion = {
         "1.9a", PR_TRUE,
@@ -431,7 +524,16 @@ static JSBool initEnvironment(void) {
     cerr << "initEnv 6a " << full_path << endl;
 
     gtk_moz_embed_set_profile_path(full_path, "TestGtkEmbed");
-#endif
+
+    single = gtk_moz_embed_single_get();
+    if (!single) {
+        fprintf(stderr, "Failed to get singleton embed object!\n");
+        exit(1);
+    }
+
+    g_signal_connect(GTK_OBJECT(single), "new_window_orphan",
+            G_CALLBACK(new_window_orphan_cb), NULL);
+
 
     cerr << "initEnv fin " << endl;
 
---
+++ tests/browser.js
@@ -0,0 +1,131 @@
+/**
+	this file shall test the functionality for exporting interfaces on the session bus.   
+ */
+LIB = "/opt/smarthome/lib/js/";
+
+load(LIB + "common.js");
+load(LIB + "shc_gui_client.js");
+
+DSO.load("dbus"); 
+DSO.load("mozembed");
+
+
+function Browser(bus) {
+	this.bus = bus;
+	this.moz = new MozEmbed();
+
+	this._iface = "at.beko.Browser";
+	this._sig =   <interface name="at.beko.Browser">
+						<method name="goto">
+						  <arg name="url" type="s" direction="in"/>
+						</method>
+						<method name="stop">
+						</method>
+						<method name="reload">
+						</method>
+						<method name="quit">
+						</method>
+						<method name="back">
+						</method>
+						<method name="forward">
+						</method>
+						<method name="fullscreen">
+						</method>
+					  </interface>;
+
+	this.goto = function(opath, url) {
+		print("JS::Browser::load("+url+")");
+		this.moz.load(url);
+	}
+
+	this.stop = function(opath) {
+		print("JS::Browser::stop()");
+		this.moz.stop();
+	}
+
+	this.reload = function(opath) {
+		print("JS::Browser::reload()");
+		this.moz.reload();
+	}
+
+	this.fullscreen = function(opath) {
+		print("JS::Browser::fullscreen()");
+		this.moz.fullscreen();
+	}
+
+	this.quit = function(opath) {
+		print("JS::Browser::quit() " + opath);
+		//Glib.quit();
+		//this.moz.quit();
+		MozEmbed.quit();
+
+	}
+
+	this.moz.setCallback(statusCb);
+}
+
+///////////////////gui client:
+
+function Client(browser) {
+	this.browser = browser;
+
+	this.play = function(arg) {
+		if (arg != null && arg[0] != null ) {
+			if ( arg[0]._url != null) {
+				this.browser.goto("/","" + arg[0]._url);
+			} else {
+				this.browser.goto("/", arg);
+			}
+		}
+	}
+
+	this.pause = function(arg) {
+		print("JS::Client::pause()");
+		print("paused browser " + arg);
+	}
+
+	this.quit = function(arg) {
+		print("JS::Client::quit() hier mit " + arg);
+		MozEmbed.quit();
+
+		return DBUS_COMMAND_OK;
+	}
+}
+
+
+bus = DBus.sessionBus();
+
+intro = new Introspectable(bus);
+ function handle(str) {
+		print("JS::Client::handle("+str+")");
+	} 
+
+browser = new Browser(bus);
+
+function statusCb(str) {
+	print("JS::Client::statusCb() " + str );
+	if (str == "quit") {
+		MozEmbed.quit();
+	}
+}
+browser.moz.setCallback(statusCb);
+
+
+print("Browser.statusCb " + browser.statusCb);
+
+intro.expose("/control", browser);
+
+guic = new GuiClient(bus);
+intro.expose(guic._path, guic);
+
+client = new Client(browser);
+guic.addClient(client);
+
+//browser.fullscreen();
+browser.goto("/","file:///opt/smarthome-src/shc/js/mozemb_js/tests/close.html");
+
+bus.requestBusName("at.beko.smarthome.Browser.Service");
+
+
+MozEmbed.main();
+
